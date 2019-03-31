9459fc9828054c22566a7cb798a3150e9f09515a
Conrad Taylor <conradwt@gmail.com>
2019-03-31 16:18:35 -0700

    Refactor schema to use resolver.

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -10,20 +10,14 @@ defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
   import Ecto.Query, only: [from: 2]
   alias PlateSlate.{Menu, Repo}
+  alias PlateSlateWeb.Resolvers
 
   query do
     @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
       arg(:matching, :string)
 
-      resolve(fn
-        _, %{matching: name}, _ when is_binary(name) ->
-          query = from t in Menu.Item, where: ilike(t.name, ^"%#{name}%")
-          {:ok, Repo.all(query)}
-
-        _, _, _ ->
-          {:ok, Repo.all(Menu.Item)}
-      end)
+      resolve(&Resolvers.Menu.menu_items/3)
     end
   end
 
