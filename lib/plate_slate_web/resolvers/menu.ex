4a398debbda983d5c9f06d6bad062be44f155697
Conrad Taylor <conradwt@gmail.com>
2019-03-31 19:20:12 -0700

    Add search resolver.

--- lib/plate_slate_web/resolvers/menu.ex
+++ lib/plate_slate_web/resolvers/menu.ex
@@ -5,6 +5,10 @@ defmodule PlateSlateWeb.Resolvers.Menu do
     {:ok, Menu.list_items(args)}
   end
 
+  def search(_, %{matching: term}, _) do
+    {:ok, Menu.search(term)}
+  end
+
   def items_for_category(category, _, _) do
     query = Ecto.assoc(category, :items)
     {:ok, PlateSlate.Repo.all(query)}
