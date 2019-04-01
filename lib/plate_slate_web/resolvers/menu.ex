31da6077c49a4216546e999f1d272a190a1d8f80
Conrad Taylor <conradwt@gmail.com>
2019-03-31 20:57:37 -0700

    Add create item resolver.

--- lib/plate_slate_web/resolvers/menu.ex
+++ lib/plate_slate_web/resolvers/menu.ex
@@ -13,4 +13,14 @@ defmodule PlateSlateWeb.Resolvers.Menu do
     query = Ecto.assoc(category, :items)
     {:ok, PlateSlate.Repo.all(query)}
   end
+
+  def create_item(_, %{input: params}, _) do
+    case Menu.create_item(params) do
+      {:error, _} ->
+        {:error, "Could not create menu item"}
+
+      {:ok, _} = success ->
+        success
+    end
+  end
 end
