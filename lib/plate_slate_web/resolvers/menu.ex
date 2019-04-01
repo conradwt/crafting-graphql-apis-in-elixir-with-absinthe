9a97a84b00675ba269ced8bc5830c9209695d493
Conrad Taylor <conradwt@gmail.com>
2019-03-31 19:14:59 -0700

    Add items_for_category function.

--- lib/plate_slate_web/resolvers/menu.ex
+++ lib/plate_slate_web/resolvers/menu.ex
@@ -4,4 +4,9 @@ defmodule PlateSlateWeb.Resolvers.Menu do
   def menu_items(_, args, _) do
     {:ok, Menu.list_items(args)}
   end
+
+  def items_for_category(category, _, _) do
+    query = Ecto.assoc(category, :items)
+    {:ok, PlateSlate.Repo.all(query)}
+  end
 end
