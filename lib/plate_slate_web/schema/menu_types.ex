8ba223c9207fe907a97f85cb935a68ddd8790e5d
Conrad Taylor <conradwt@gmail.com>
2019-03-31 19:15:47 -0700

    Add the search_result union.

--- lib/plate_slate_web/schema/menu_types.ex
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -1,6 +1,32 @@
 defmodule PlateSlateWeb.Schema.MenuTypes do
   use Absinthe.Schema.Notation
 
+  alias PlateSlateWeb.Resolvers
+
+  object :category do
+    field :name, :string
+    field :description, :string
+
+    field :items, list_of(:menu_item) do
+      resolve(&Resolvers.Menu.items_for_category/3)
+    end
+  end
+
+  union :search_result do
+    types([:menu_item, :category])
+    # Almost done...
+    resolve_type(fn
+      %PlateSlate.Menu.Item{}, _ ->
+        :menu_item
+
+      %PlateSlate.Menu.Category{}, _ ->
+        :category
+
+      _, _ ->
+        nil
+    end)
+  end
+
   @desc "Filtering options for the menu item list"
   input_object :menu_item_filter do
     # menu item filter fields
