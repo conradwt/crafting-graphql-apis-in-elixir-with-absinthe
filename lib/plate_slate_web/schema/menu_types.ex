5d9413474c678a5a34f35369c0cc68f7a287af5b
Conrad Taylor <conradwt@gmail.com>
2019-03-31 21:21:51 -0700

    Add missing price field to the menu item objecy.

--- lib/plate_slate_web/schema/menu_types.ex
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -59,6 +59,7 @@ defmodule PlateSlateWeb.Schema.MenuTypes do
     field :id, :id
     field :name, :string
     field :description, :string
+    field :price, :decimal
     field :added_on, :date
   end
 
