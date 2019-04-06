1f06d0388015a3e10d7053eb8cb7c0db514ed6ea
Conrad Taylor <conradwt@gmail.com>
2019-04-05 21:29:36 -0700

    Add test case for new order.

---
+++ test/plate_slate_web/schema/subscription/new_order_test.exs
@@ -0,0 +1,40 @@
+defmodule PlateSlateWeb.Schema.Subscription.NewOrderTest do
+  use PlateSlateWeb.SubscriptionCase
+
+  @subscription """
+  subscription {
+    newOrder {
+      customerNumber
+    }
+  }
+  """
+  @mutation """
+  mutation ($input: PlaceOrderInput!) {
+    placeOrder(input: $input) { order { id } }
+  }
+  """
+  test "new orders can be subscribed to", %{socket: socket} do
+    # setup a subscription
+    ref = push_doc(socket, @subscription)
+    assert_reply ref, :ok, %{subscriptionId: subscription_id}
+
+    # run a mutation to trigger the subscription
+    order_input = %{
+      "customerNumber" => 24,
+      "items" => [%{"quantity" => 2, "menuItemId" => menu_item("Reuben").id}]
+    }
+
+    ref = push_doc(socket, @mutation, variables: %{"input" => order_input})
+    assert_reply ref, :ok, reply
+    assert %{data: %{"placeOrder" => %{"order" => %{"id" => _}}}} = reply
+
+    # check to see if we got subscription data
+    expected = %{
+      result: %{data: %{"newOrder" => %{"customerNumber" => 24}}},
+      subscriptionId: subscription_id
+    }
+
+    assert_push "subscription:data", push
+    assert expected == push
+  end
+end
