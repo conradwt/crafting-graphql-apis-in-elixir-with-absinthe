fa996fd3e4a7c5f04ec822e4595b08e9c6755382
Conrad Taylor <conradwt@gmail.com>
2019-04-05 17:23:47 -0700

    Add the ordering resolver.

---
+++ lib/plate_slate_web/resolvers/ordering.ex
@@ -0,0 +1,29 @@
+defmodule PlateSlateWeb.Resolvers.Ordering do
+  alias PlateSlate.Ordering
+
+  def place_order(_, %{input: place_order_input}, _) do
+    case Ordering.create_order(place_order_input) do
+      {:ok, order} ->
+        {:ok, %{order: order}}
+
+      {:error, changeset} ->
+        {:ok, %{errors: transform_errors(changeset)}}
+    end
+  end
+
+  defp transform_errors(changeset) do
+    changeset
+    |> Ecto.Changeset.traverse_errors(&format_error/1)
+    |> Enum.map(fn
+      {key, value} ->
+        %{key: key, message: value}
+    end)
+  end
+
+  @spec format_error(Ecto.Changeset.error()) :: String.t()
+  defp format_error({msg, opts}) do
+    Enum.reduce(opts, msg, fn {key, value}, acc ->
+      String.replace(acc, "%{#{key}}", to_string(value))
+    end)
+  end
+end
