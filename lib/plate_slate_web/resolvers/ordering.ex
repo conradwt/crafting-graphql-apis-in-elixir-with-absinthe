03159601ca64bef9fa31f3bcf939fae279db0ce3
Conrad Taylor <conradwt@gmail.com>
2019-04-05 20:36:16 -0700

    Update ordering resolver to publish an event when new order is placed.

--- lib/plate_slate_web/resolvers/ordering.ex
+++ lib/plate_slate_web/resolvers/ordering.ex
@@ -4,6 +4,7 @@ defmodule PlateSlateWeb.Resolvers.Ordering do
   def place_order(_, %{input: place_order_input}, _) do
     case Ordering.create_order(place_order_input) do
       {:ok, order} ->
+        Absinthe.Subscription.publish(PlateSlateWeb.Endpoint, order, new_order: "*")
         {:ok, %{order: order}}
 
       {:error, changeset} ->
