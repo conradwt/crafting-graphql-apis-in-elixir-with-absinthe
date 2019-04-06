bb07caa6bb633837d5d5efa690f8e3ca719c0cef
Conrad Taylor <conradwt@gmail.com>
2019-04-05 17:20:01 -0700

    Add place order mutation.

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -27,6 +27,11 @@ defmodule PlateSlateWeb.Schema do
   end
 
   mutation do
+    field :place_order, :order_result do
+      arg(:input, non_null(:place_order_input))
+      resolve(&Resolvers.Ordering.place_order/3)
+    end
+
     field :create_menu_item, :menu_item_result do
       arg(:input, non_null(:menu_item_input))
       resolve(&Resolvers.Menu.create_item/3)
